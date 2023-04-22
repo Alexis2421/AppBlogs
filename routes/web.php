03a074b384703e6fc0be2062e47c9df4e275f85a
Alexis2421 <79373108+Alexis2421@users.noreply.github.com>
2023-04-22 14:26:34 -0500

    Cargar Corrección de las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::resource('blog', \App\Http\Controllers\BlogController::class);
+Route::group(['middleware' => 'auth'], function (){
+    Route::get('/', [BlogController::class, 'index'])->name('home');
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('blog', \App\Http\Controllers\BlogController::class);
+
+
