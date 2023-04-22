2a7046b8ba07b56f089d4b39a6cbae862829aec4
Alexis2421 <79373108+Alexis2421@users.noreply.github.com>
2023-04-22 11:02:47 -0500

    Cargar primer requerimiento del usuario por defecto

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Encryption\Encrypter;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -22,6 +26,11 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+      //  $data = array("name" => "Alexis Pescador", "age" =>"26", "type"=>"1", "email"=>"alexis.pescador@ucp.edu.co", "email_verified_at" => "alexis.pescador@ucp.edu.co","password"=>Crypt::encryptString('Alexis2023'));
+      $password = '$2y$10$Yz3btxEhQGXJ2.nXMU9f3u9MnOWGIuq7KeQ4JIVvM3jLR1CEFMwjm';
+      $data = array("name" => "Alexis Pescador", "age" =>"26", "type"=>"1", "email"=>"alexis.pescador@ucp.edu.co", "email_verified_at" => "alexis.pescador@ucp.edu.co","password"=>$password);
+       User::create($data);
     }
 
     /**
