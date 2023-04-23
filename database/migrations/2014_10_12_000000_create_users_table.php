c79203a49a01fcc1d6e06b6fd2df1431e729e8f7
Alexis2421 <79373108+Alexis2421@users.noreply.github.com>
2023-04-23 13:22:11 -0500

    Modificación de la tabla users, se quitó el default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('age');
-            $table->integer('type')->default('0');
+            $table->integer('type');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
